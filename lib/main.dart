c05b88a1fc53b9abed5cbb504ca8828dd749f23b
Spectari-code <SpectariVoid@gmail.com>
2023-02-20 09:48:22 +0200

    Start of quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Hello!'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz App"),
+        ),
+        body: Column(children: <Widget>[
+          Text("The question:"),
+          ElevatedButton(onPressed: null, child: Text("Answer 1")),
+          ElevatedButton(onPressed: null, child: Text("Answer 2")),
+          ElevatedButton(onPressed: null, child: Text("Answer 3")),
+        ]),
+      ),
     );
   }
 }
