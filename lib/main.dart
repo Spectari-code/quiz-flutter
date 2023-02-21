1145a9f867c7ebf1a3fb138b3924afec005c2360
Spectari-code <SpectariVoid@gmail.com>
2023-02-21 15:51:23 +0200

    Convert to stateful, need to debug list range

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
 
   void answerQuestion() {
-    print("Answer chosen!");
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Quiz App"),
         ),
         body: Column(children: <Widget>[
-          Text("The question:"),
+          Text(questions[questionIndex]),
           // Pass pointer of answerQuestion not answerQuestion() as that will
           // excecute the function when passed instead of waiting for button to
           // be pressed. In short, dont use (), just use the name.
